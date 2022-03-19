680c4e27267f0f7eb9e25dc16507c46f12828ae2
kang sheng <kangsheng0721@gmail.com>
2022-03-19 14:55:44 +0800

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import Axios from "axios"
+
 
 function App() {
+  const[pokemon,setpokemon]=React.useState("");
+  const[pokemonChosen,setpokemonchose]=React.useState(false)
+  const[stats,setstats]=React.useState({
+      name:"",
+      species:"",
+      img:"",
+      hp:"",
+      attack:"",
+      defense:"",
+      type:"",
+    });
+
+
+
+
+
+  const searchPokemon=()=>{
+    // make the request
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(
+      (response)=>{
+          setstats({name:pokemon,
+            species:response.data.species.name,
+            img:response.data.sprites.front_default,
+            hp:response.data.stats[0].base_stat,
+            attack:response.data.stats[1].base_stat,
+            defense:response.data.stats[2].base_stat,
+            type:response.data.types[0].type.name,
+          })
+          setpokemonchose(true)
+      }
+
+
+    );
+
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TitleSession">
+        <h1>Pokemon Stats</h1>
+        <input type="text" onChange={(event)=>{
+          setpokemon(event.target.value)
+        }}/>
+        <button onClick={searchPokemon}>Search your desire pokemon</button>
+        </div>
+        <div className='display-section'>
+          {!pokemonChosen ?(
+          <h1>Please enter a pokemon</h1>):
+          (
+          <>
+          <h1>{stats.name}</h1>
+          <img src={stats.img} alt="true"/>
+          <p>
+            {`HP: ${stats.hp}`} <br></br>
+            {`Attack: ${stats.attack}`} <br></br>
+            {`Defense: ${stats.defense}`} <br></br>
+            {`Type: ${stats.type}`} <br></br>
+          </p>
+          
+          </>
+          )}
+        </div>
     </div>
   );
 }
